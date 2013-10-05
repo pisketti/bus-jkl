a40b9cb1a015311279527b15b1f40035461d6b25
Tero Kadenius <tero.kadenius@gmail.com>
2013-10-05 23:58:08 +0300

    Logic for handling converting line numbers in request to a seq of numbers

--- src/bus_jkl/request.clj
+++ src/bus_jkl/request.clj
@@ -1,7 +1,39 @@
 (ns bus-jkl.request
-  (:require [bus-jkl.util :as util]))
+  (:require [bus-jkl.util :as util]
+            [clojure.string :as s]))
 
-(defn line-numbers-to-seq[{:keys [numbers] :as params}]
+(defn drop-middle [string]
+  (str (first string) (last string)))
+
+(defn vec-or-list? [string]
+  (#{"[]" "()"} (drop-middle string)))
+
+(defn components [seq-str]
+  (s/split seq-str #","))
+
+(defn drop-parens [string]
+  (if (vec-or-list? string)
+    (->> string
+         rest
+         butlast
+         (apply str))
+    string))
+
+(defn remove-whitespace [string]
+  (s/replace string #"\s" ""))
+
+(defn int-str? [string]
+  (util/parse-int string))
+
+(defn parse-seq [string]
+  (->> string
+       remove-whitespace
+       drop-parens
+       components
+       (filter int-str?)
+       (map util/parse-int)))
+
+(defn line-numbers-to-seq [{:keys [numbers] :as params}]
   (if numbers
-    (assoc params :numbers (util/parse-int numbers))
+    (assoc params :numbers (parse-seq numbers))
     params))
--- test/bus_jkl/request_test.clj
+++ test/bus_jkl/request_test.clj
@@ -10,7 +10,24 @@
 (fact "Request has no numbers. Returns the map untouched"
   (line-numbers-to-seq {}) => {})
 
+(fact "Request has numbers with invalid value. Returns an empty number seq"
+  (line-numbers-to-seq {:numbers "x"}) => {:numbers []})
+
+(fact "Request has empty numbers. Returns an empty number seq"
+  (line-numbers-to-seq {:numbers ""}) => {:numbers []}
+  (line-numbers-to-seq {:numbers " "}) => {:numbers []}
+  (line-numbers-to-seq {:numbers "[]"}) => {:numbers []}
+  (line-numbers-to-seq {:numbers "()"}) => {:numbers []})
+
 (fact "Request has one number x as string. Returns the map with :numbers [x] where x converted to str"
-  (line-numbers-to-seq {:numbers "9"}) => {:numbers 9}
-  ;(line-numbers-to-seq {:numbers "[9]"}) => {:numbers 9}
-  )
+  (line-numbers-to-seq {:numbers "9"}) => {:numbers [9]}
+  (line-numbers-to-seq {:numbers " 9 "}) => {:numbers [9]}
+  (line-numbers-to-seq {:numbers "[9]"}) => {:numbers [9]}
+  (line-numbers-to-seq {:numbers " [ 9 ] "}) => {:numbers [9]}
+  (line-numbers-to-seq {:numbers " [ 8 9 ] "}) => {:numbers [89]})
+
+(fact "Request has several numbers as string. Returns the map with items converted to seq as numbers"
+  (line-numbers-to-seq {:numbers "9,10"}) => {:numbers [9 10]}
+  (line-numbers-to-seq {:numbers "(9,10)"}) => {:numbers [9 10]}
+  (line-numbers-to-seq {:numbers "[9,10]"}) => {:numbers [9 10]}
+  (line-numbers-to-seq {:numbers "[ 1 1 , 2 2 ]"}) => {:numbers [11 22]})
